4aa1ed9943b398078ac197e6d5643c30238bef2e
Ridvan <ridvans@abv.bg>
2023-06-08 12:50:04 +0300

    updating

--- back-end/NoSQL_and_MongoDB/05. JS-Back-End-NoSQL-and-MongoDB/mongodb/index.js
+++ back-end/NoSQL_and_MongoDB/05. JS-Back-End-NoSQL-and-MongoDB/mongodb/index.js
@@ -1,16 +1,16 @@
-const mongodb = require('mongodb');
+const mongodb = require("mongodb");
 
-const connectionStr = 'mongodb://127.0.0.1:27017';
+const connectionStr = "mongodb://localhost:27017";
 
 const client = new mongodb.MongoClient(connectionStr);
 
 async function connectDb() {
-    await client.connect();
-    const db = client.db('catShelter');
-    const cats = db.collection('cats');
-    const result = await cats.find().toArray();
+  await client.connect();
+  const db = client.db("catShelter");
+  const cats = db.collection("cats");
+  const result = await cats.find().toArray();
 
-    console.log(result);
+  console.log("successfully connected");
 }
 
 connectDb();
