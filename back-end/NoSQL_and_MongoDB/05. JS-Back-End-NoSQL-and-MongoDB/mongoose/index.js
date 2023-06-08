db9eccd0a28fc42d1f442daf462f86674c4b5699
Ridvan <ridvans@abv.bg>
2023-06-08 13:42:52 +0300

    updating

--- back-end/NoSQL_and_MongoDB/05. JS-Back-End-NoSQL-and-MongoDB/mongoose/index.js
+++ back-end/NoSQL_and_MongoDB/05. JS-Back-End-NoSQL-and-MongoDB/mongoose/index.js
@@ -1,65 +1,69 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const Cat = require('./models/Cat');
-const Person = require('./models/Person');
+const Cat = require("./models/Cat");
+const Person = require("./models/Person");
 
 async function connectDb() {
-    await mongoose.connect('mongodb://127.0.0.1:27017/catShelter');
+  await mongoose.connect("mongodb://localhost:27017/catShelter");
 
-    console.log('Db Connected successfully');
+  console.log("Db Connected successfully");
 
-    // cats.forEach(cat => cat.greet()); // instance method
-    // cats.forEach(cat => console.log(cat.info)); // virtual property
-    // const result = await Cat.giveMeCats() // Static model method
+  // cats.forEach(cat => cat.greet()); // instance method
+  // cats.forEach(cat => console.log(cat.info)); // virtual property
+  // const result = await Cat.giveMeCats() // Static model method
 
-    // Read
-    // const cats = await Cat.find({ breed: 'Angora' });
-    // const cats = await Cat.findOne({breed: 'Angora'});
-    // const cats = await Cat.findById('646f8c5265a096882c26c345');
+  // Read
+  // const cats = await Cat.find({ breed: 'Angora' });
+  // const cats = await Cat.findOne({breed: 'Angora'});
+  //   const cat = await Cat.findById("6481a5d74dc5e43f3f1e1c6e");
+  //   console.log(cat);
 
-    // Create method 1
-    // const newCat = new Cat({
-    //     name: 'Zu',
-    //     age: 10,
-    //     breed: 'Ulichna'
-    // });
-    // await newCat.save();
+  // Create method 1
+  // const newCat = new Cat({
+  //     name: 'Zu',
+  //     age: 10,
+  //     breed: 'Ulichna'
+  // });
+  // await newCat.save();
 
-    // Create method 2
-    // const newCat = await Cat.create({
-    //     age: 6,
-    //     name: 'Charli',
-    //     color: 'Red',
-    //     breed: 'Dog',
-    // });
+  // Create method 2
+  // const newCat = await Cat.create({
+  //     age: 6,
+  //     name: 'Charli',
+  //     color: 'Red',
+  //     breed: 'Dog',
+  // });
+  const gasper = await Cat.findById("6481af0c7b4d0b97bd0321fd");
+  gasper.name = "Casper";
+  console.log(gasper.question);
 
-    // Update method 1
-    // const charli = await Cat.findOne({name: 'Charli'});
-    // charli.age = 10;
-    // await charli.save();
+  // Update method 1
+  // const charli = await Cat.findOne({name: 'Charli'});
+  // charli.age = 10;
+  // await charli.save();
 
-    // Update method 2 native mongoDb
-    // await Cat.updateOne({ name: 'Charli' }, { $set: { age: 9 } });
+  // Update method 2 native mongoDb
+  // await Cat.updateOne({ name: 'Charli' }, { $set: { age: 9 } });
 
-    // Update method 3 the mongoose extension
-    // await Cat.findByIdAndUpdate('646fa6c4c383c3fd53c0194a', {$set: {breed: 'Chihuahua'}});
+  // Update method 3 the mongoose extension
+  // await Cat.findByIdAndUpdate('646fa6c4c383c3fd53c0194a', {$set: {breed: 'Chihuahua'}});
 
-    // Delete method 1
-    // await Cat.deleteOne({name: 'Zuza'});
+  // Delete method 1
+  // await Cat.deleteOne({name: 'Zuza'});
 
-    // Delete method 2
-    // await Cat.findByIdAndDelete('646f8c5265a096882c26c345');
+  // Delete method 2
+  // await Cat.findByIdAndDelete('646f8c5265a096882c26c345');
 
-    // Creating collection by creating first record in non existant collection
-    // await Person.create({
-    //     name: 'Pesho',
-    //     age: 20,
-    // });
+  // Creating collection by creating first record in non existant collection
+  // await Person.create({
+  //     name: 'Pesho',
+  //     age: 20,
+  // });
 
-    // Find all non angora cats
-    // const cats = await Cat.find({ breed: { $ne: 'Angora' } });
-    // const cats = await Cat.find().where('breed').ne('Angora');
-    // console.log(cats);
+  // Find all non angora cats
+  // const cats = await Cat.find({ breed: { $ne: 'Angora' } });
+  // const cats = await Cat.find().where('breed').ne('Angora');
+  // console.log(cats);
 }
 
 connectDb();
--- back-end/NoSQL_and_MongoDB/05. JS-Back-End-NoSQL-and-MongoDB/mongoose/models/Cat.js
+++ back-end/NoSQL_and_MongoDB/05. JS-Back-End-NoSQL-and-MongoDB/mongoose/models/Cat.js
@@ -1,39 +1,42 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const catSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, 'Name is required!'],
-        minLength: [3, 'Custom min length message'],
-        maxLength: 20,
+  name: {
+    type: String,
+    required: [true, "Name is required!"],
+    minLength: [3, "Custom min length message"],
+    maxLength: 20,
+  },
+  color: {
+    type: String,
+    require: false,
+    enum: {
+      values: ["white", "black", "yellow"],
+      message: "{VALUE} is wrong color",
     },
-    color: {
-        type: String,
-        require: false,
-        enum: {
-            values: ['white', 'black', 'yellow'],
-            message: '{VALUE} is wrong color',
-        }
-    },
-    age: Number,
-    breed: String,
+  },
+  age: Number,
+  breed: String,
 });
 
 // Instance method
-catSchema.methods.greet = function() {
-    console.log(`Hello I\'m cat - my name is ${this.name}`);
+catSchema.methods.greet = function () {
+  console.log(`Hello I\'m cat - my name is ${this.name}`);
 };
 
 // Virtual property
-catSchema.virtual('info').get(function() {
-    return `My name is ${this.name} and I'm ${this.age} years old`;
+catSchema.virtual("info").get(function () {
+  return `My name is ${this.name} and I'm ${this.age} years old`;
 });
 
 // Static model method
-catSchema.static('giveMeCats', function() {
-    return this.find();
+catSchema.static("giveMeCats", function () {
+  return this.find();
+});
+catSchema.virtual("question").get(function () {
+  return `Will you play with me.........pls`;
 });
 
-const Cat = mongoose.model('Cat', catSchema);
+const Cat = mongoose.model("Cat", catSchema);
 
 module.exports = Cat;
