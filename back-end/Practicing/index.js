3e67b2b5f64ac3627fd64bb3ab84e5ac2df7e316
Ridvan <ridvans@abv.bg>
2023-06-15 14:31:21 +0300

    updating

--- back-end/Practicing/index.js
+++ back-end/Practicing/index.js
@@ -1,9 +1,9 @@
 const env = process.env.NODE_ENV || 'development';
 
-const config = require('./config/config')[env];
+const config = require('./src/config/config')[env];
 const app = require('express')();
 
-require('./config/express')(app);
-require('./config/routes')(app);
+require('./src/config/express')(app);
+require('./src/config/routes')(app);
 
 app.listen(config.port, console.log(`Listening on port ${config.port}! Now its up to you...`));
