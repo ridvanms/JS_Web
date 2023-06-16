d4b62984839c00fee7e885b7b15dbe164b669967
Ridvan <ridvans@abv.bg>
2023-06-16 10:46:48 +0300

    updating

--- back-end/Practicing/src/config/express.js
+++ back-end/Practicing/src/config/express.js
@@ -1,13 +1,13 @@
-const express = require('express');
-const handlebars = require('express-handlebars');
-const bodyParser = require('body-parser');
+const express = require("express");
+const handlebars = require("express-handlebars");
+const bodyParser = require("body-parser");
+const path = require("path");
 
 module.exports = (app) => {
-    
-    //TODO: Setup the view engine
+  //TODO: Setup the view engine
+  app.set("views", path.join(__dirname));
+  app.set(handlebars, "hbs");
+  //TODO: Setup the body parser
 
-    //TODO: Setup the body parser
-
-    //TODO: Setup the static files
-
+  //TODO: Setup the static files
 };
